0c314a516aa2115fdd7a2a7934420e89eb82d20c
esraa <esraa2023112@gmail.com>
2024-10-31 21:08:15 +0300

    done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''0-validate_utf8.py'''
+
+
+def validUTF8(data):
+    '''Checks if a list of integers are valid UTF-8 codepoints.'''
+    num_bytes_following = 0
+
+    for num in data:
+        if num_bytes_following:
+            if num >> 6 != 0b10:
+                return False
+            num_bytes_following -= 1
+        else:
+            if num >> 7 == 0:
+                num_bytes_following = 0
+            elif num >> 5 == 0b110:
+                num_bytes_following = 1
+            elif num >> 4 == 0b1110:
+                num_bytes_following = 2
+            elif num >> 3 == 0b11110:
+                num_bytes_following = 3
+            else:
+                return False
+
+    return num_bytes_following == 0
