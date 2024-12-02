9ed2cccb0552e4a8a88ddcdbf99caadba55e5093
esraa <esraa2023112@gmail.com>
2024-12-02 16:07:20 +0200

    done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/bin/usr/python3
+'''Island parameter'''
+def island_perimeter(grid):
+    ''' funaction that calculate the parameter of the island'''
+    visit = set()
+
+    def dfs(i, j):
+        ''' function that return perimeter'''
+        if i >= len(grid) or j >= len(grid[0]) or \
+            i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visit:
+            return 0
+        visit.add((i, j))
+        per = dfs(i, j + 1)
+        per += dfs(i + 1, j)
+        per += dfs(i, j - 1)
+        per += dfs(i - 1, j)
+        return per
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
